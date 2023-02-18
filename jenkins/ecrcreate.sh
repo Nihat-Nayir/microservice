9d7dde7fec23dfc4071963cb817fad95cdbd3183
Nihat NAYİR <109424781+Nihat-Nayir@users.noreply.github.com>
2023-02-18 10:02:15 +0300

    Delete ecrcreate.sh

--- jenkins/ecrcreate.sh
+++
@@ -1,9 +0,0 @@
-PATH="$PATH:/usr/local/bin"
-APP_REPO_NAME="clarusway-repo/petclinic-app-dev1" # Write your own repo name
-AWS_REGION="us-east-1" #Update this line if you work on another region
-ECR_REGISTRY="286083324559.dkr.ecr.us-east-1.amazonaws.com" # Replace this line with your ECR name
-aws ecr create-repository \
-    --repository-name ${APP_REPO_NAME} \
-    --image-scanning-configuration scanOnPush=false \
-    --image-tag-mutability MUTABLE \
-    --region ${AWS_REGION}
